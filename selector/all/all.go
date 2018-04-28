b3aa374c0ab87c561911a478bcf22f49826767f4
Sander Tiganik <sander.tiganik@gmail.com>
2018-04-28 11:42:14 +0300

    selector/all: Add convenience import package for selection algorithms.

---
+++ selector/all/all.go
@@ -0,0 +1,12 @@
+/*
+Package all is a convenience package for importing all selection algorithms
+implemented in this project.
+*/
+package all
+
+import (
+	// Blank importing all algorithms forces the algorithms to register
+	// themselves at runtime saving the trouble of having to import all
+	// algorithms one by one.
+	_ "github.com/stiganik/gap/selector/scx"
+)
